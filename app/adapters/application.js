617160055d04ea1f905b8cd1b65a467b2b0091f0
Chris Anderson <codepoetchris@gmail.com>
2017-05-09 18:51:53 -0700

    Ported to ember sans socket.io
    Ported to ember sans socket.io

---
+++ app/adapters/application.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+// App.ApplicationAdapter = DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:3002',
+  namespace: 'api'
+});
+
+// export default DS.JSONAPIAdapter.extend({
+//   host: 'http://localhost:3002',
+//   namespace: 'api'
+// });
